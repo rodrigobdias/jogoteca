cdce38dc390a7f0bedec841973a59be6842b2577
Rodrigo <rodrigobdias@gmail.com>
2019-05-13 16:08:09 -0300

    Autenticando usuários com sessão do Flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'alura'
+
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -19,6 +21,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -29,6 +33,29 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logado com sucesso!')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
+    else:
+        flash('Username or password invalid !')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect('/')
+
 
 app.run(debug=True)
 
